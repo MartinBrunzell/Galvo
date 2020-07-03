e969c2c578217c6af77d19a50404f0533510d3bf
Theodor Staffas <37420485+tstaffas@users.noreply.github.com>
2020-07-03 10:14:56 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -2,6 +2,57 @@ import DP832 as DP
 import numpy as np
 import time
 
+class Motor(power_supply)
+    def __init__(self, power_supply):
+        self.power_supply = power_supply
+
+
+    def set_output(self, v):
+    ## Enables the output of negative voltages.
+    ## Made to work for the Rigol DP832 using two channels.
+    #  v            - Desired output (-10 to 10 V).
+    #  power_supply - Instanced class of control functions for a power supply.
+    if np.abs(v) <= 10:
+        if v < 0:
+            self.power_supply.set_voltage(2,v)
+            self.power_supply.set_voltage(1,0)
+        elif v >= 0:
+            self.power_supply.set_voltage(2,0)
+            self.power_supply.set_voltage(1,v)
+    else:
+        print("ERROR: Voltage limit exceeded (±10V max)")
+
+
+    
+    def scan_output(self, start_V, stop_V, N_steps, t_delay = 0):
+        ## Scans the voltage from start_V to stop_V with N_steps ammount of steps
+        #  power_supply - Instanced class of control functions for a power supply.
+        #  t_delay      - sets the delay in time between each voltage step
+
+        if np.abs(start_V) <= 10 and np.abs(stop_V) <= 10:
+            voltages = np.linspace(start_V,stop_V,N_steps)
+            for v in voltages:
+                self.set_output(v, self.power_supply)
+                time.sleep(t_delay)
+        else:
+            print("ERROR: Voltage limit exceeded (±10V max)")
+
+
+def trace_curve(MotorX, MotorY):
+
+    vx = 10
+    vy = 4
+
+    N_steps_y = 20
+    N_steps_x = 51
+
+    for v in range(-vy,vy,(2*vy/N_steps_y)):
+        MotorX.scan_output(-vx,vx,N_steps_x)
+        
+    
+    
+    
+
 def set_output(v,power_supply):
     ## Enables the output of negative voltages.
     ## Made to work for the Rigol DP832 using two channels.
